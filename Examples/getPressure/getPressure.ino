90b6740073e02d5bb25f07bae176048e145c4784
shtarbanov <alims@media.mit.edu>
2019-11-25 21:59:58 -0500

    Example Update

--- Examples/getPressure/getPressure.ino
+++ Examples/getPressure/getPressure.ino
@@ -9,16 +9,22 @@ void setup() {
     Serial.println("Sensor not found.");
     while(1) delay(10);
   }
+  mpr.setPressureUnit(HPA);
 }
 
 void loop() {
-  //TODO: Can you read the pressure at the 400MHz bus speed?  
-  float pPSI = mpr.getPressurePSI();
-  //delay(5); This is no longer necessary, b/c I fixed the library to check if status byte is busy!
-  float pATM = mpr.getPressureATM();
-  float pHPA = mpr.getPressureHPA();
+  //TODO: Can you read the pressure at the 400MHz bus speed?
+  float pressure = mpr.getPressure(); //this returns in the unit we have specified in the setPressureUnit() function.
+  Serial.print("\nPressure: "); Serial.println(pressure); 
+  Serial.println();
+
+  //We can also force a specific unit in the getPressure() function.
+  float pPSI = mpr.getPressure(PSI);
+  float pATM = mpr.getPressure(ATM);
+  float pHPA = mpr.getPressure(HPA);
   Serial.print("Pressure (PSI): "); Serial.println(pPSI);
   Serial.print("Pressure (ATM): "); Serial.println(pATM);
   Serial.print("Pressure (HPA): "); Serial.println(pHPA);
+
   delay(1000);
 }
